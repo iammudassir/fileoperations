29ef9c6a20911e531087b0f84d1bcda2340a6740
Mudassir Chimawkar <mudassirchimawkar@gmail.com>
2022-11-28 13:00:41 +0530

    Update FileTest.py

--- FileTest.py
+++ FileTest.py
@@ -7,10 +7,10 @@ data = json.load(f)
 arr=[]
 
 for i in data['data']:
-	print(i['sha'])
-	arr.append(i['sha'])
+	print(i['test'])
+	arr.append(i['test']) #replace this with the string to search
 f.close()
-print ("Total Container SHA Count:"+str(len(arr)))
+print ("Total:"+str(len(arr)))
 print("Duplicate elements in given array: ");
 
 for i in range(0, len(arr)):
